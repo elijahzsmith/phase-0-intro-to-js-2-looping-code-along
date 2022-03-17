f82dc0664341bd308243097e4279a336afb77ad9
Elijah Smith <elijahsmith.m@gmail.com>
2022-03-16 23:57:30 -0500

    commit tree aff8e6b12280639054f1c39e118af1445042cbc7

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const arrOfStr = [];
+
+// For loop
+const writeCards = (arrOfNames, name) => {
+    let newArr = [];
+    for (let i = 0; i < arrOfNames.length; i++) {
+        newArr.push(`Thank you, ${arrOfNames[i]}, for the wonderful ${name} gift!`)
+    }
+    return newArr;
+}
+
+// While loop 
+const countDown = n => {
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
